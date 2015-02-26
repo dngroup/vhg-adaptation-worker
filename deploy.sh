ca1cd2268cf2a42e2733b971510cad96f820a4fa
dbourasseau <dbourasseau@enseirb-matmeca.fr>
2015-02-26 14:18:30 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 #Script for Ubuntu 14.04
 
 #Install RabbitMQ for Celery, Then Celery and MediaInfo wrapper for python
-apt-get install rabbitmq-server python-pip mediainfo python-lxml python-dev wget
+apt-get -y install rabbitmq-server python-pip mediainfo python-lxml python-dev wget
 pip install celery pymediainfo
 #Compile Manually MP4Box from GPAC project
 ./compile_MP4Box.sh
