2a538b9f6e00f1c9b9f1403d7880ae76d012ed36
dbourasseau <dbourasseau@enseirb-matmeca.fr>
2016-01-07 17:44:23 +0100

    test hard workflow

--- test/hard.sh
+++ test/hard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd soft
+cd hard
 docker-compose kill
 docker-compose up -d
 wget --tries=0 http://localhost:25672/cli/rabbitmqadmin -O rabbitmqadmin
@@ -9,7 +9,7 @@ chmod +x rabbitmqadmin
 echo ---------------------------------------------
 #./rabbitmqadmin --port=25672 -f long -d 3 list queues
 echo send request
-./rabbitmqadmin --port=25672 publish exchange=amq.default routing_key=soft properties='{"content_encoding":"utf-8", "content_type":"application/json"}'  payload='{"id":"testsoft","task":"adaptation.commons.encode_workflow","args":[],"kwargs":{"url":"http://nginx/1.mp4","qualities":{"quality":[{"name":"lowx264","bitrate":500,"codec":"libx264","height":320},{"name":"lowx265","bitrate":250,"codec":"libx265","height":320}]}},"retries":1,"eta":"2015-12-09T07:54:42+01:00"}'
+./rabbitmqadmin --port=25672 publish exchange=amq.default routing_key=hard properties='{"content_encoding":"utf-8", "content_type":"application/json"}'  payload='{"id":"testhard","task":"adaptation.commons.encode_workflow_hard","args":[],"kwargs":{"url":"http://nginx/1.mp4","qualities":{"quality":[{"name":"h264_gpu","bitrate":500,"codec":"h264_gpu","height":320},{"name":"h265_gpu","bitrate":250,"codec":"h265_gpu","height":320}]}},"retries":1,"eta":"2015-12-09T07:54:42+01:00"}'
 echo $?
 echo ---------------------------------------------
 echo ---------------------------------------------
@@ -25,23 +25,22 @@ if [[ "$result" == "a3b9f93268cc8b5e4a09bda51d19f85e" ]]; then
     result=$(./rabbitmqadmin --port=25672 get queue=transcode-result requeue=false | grep transcode-result |  sed "s/.* | \({.*} \).*|/\1/g"|python -m json.tool|grep md5|sed "s/.*md5\": \"\([0-9a-fA-F]*\)\".*/\1/g")
     echo $result
 
-    if [[ "$result" == "e46f43484d4bbc2b21b1900e930143ee" ]]; then
+    if [[ "$result" == "a3b9f93268cc8b5e4a09bda51d19f85e" ]]; then
         echo "ok"
         result=$(./rabbitmqadmin --port=25672 get queue=transcode-result requeue=false | grep transcode-result |  sed "s/.* | \({.*} \).*|/\1/g"|python -m json.tool|grep md5|sed "s/.*md5\": \"\([0-9a-fA-F]*\)\".*/\1/g")
         echo $result
 
-        if [[ "$result" == "56da1b97b62c53f5d50e369b2b0c7462" ]]; then
+        if [[ "$result" == "a3b9f93268cc8b5e4a09bda51d19f85e" ]]; then
             echo "ok"
         else
             echo "not ok";
             exit -1
         fi
-
+        exit 0
     else
         echo "not ok";
         exit -1
     fi
-    docker-compose stop
     exit 0
 else
     echo "not ok";
