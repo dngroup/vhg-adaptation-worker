65e447aa0b6fd797e854cd899e9cbf5397ec6cbb
Nicolas Herbaut <nherbaut@labri.fr>
2016-01-08 09:55:54 +0100

    done with @dbourasseau

--- test/soft.sh
+++ test/soft.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 cd soft
-docker-compose kill
-docker-compose up -d
+QUEUE=soft docker-compose kill
+QUEUE=soft docker-compose up -d
 wget --tries=0 http://localhost:25672/cli/rabbitmqadmin -O rabbitmqadmin
 chmod +x rabbitmqadmin
 
 echo ---------------------------------------------
 #./rabbitmqadmin --port=25672 -f long -d 3 list queues
 echo send request
+sleep 3
 ./rabbitmqadmin --port=25672 publish exchange=amq.default routing_key=soft properties='{"content_encoding":"utf-8", "content_type":"application/json"}'  payload='{"id":"testsoft","task":"adaptation.commons.encode_workflow","args":[],"kwargs":{"url":"http://nginx/1.mp4","qualities":{"quality":[{"name":"lowx264","bitrate":500,"codec":"libx264","height":320},{"name":"lowx265","bitrate":250,"codec":"libx265","height":320}]}},"retries":1,"eta":"2015-12-09T07:54:42+01:00"}'
 echo $?
 echo ---------------------------------------------
@@ -16,7 +17,7 @@ echo ---------------------------------------------
 echo ---------------------------------------------
 echo ---------------------------------------------
 #./rabbitmqadmin --port=25672 get queue=transcode-result
-sleep 15
+sleep 30
 result=$(./rabbitmqadmin --port=25672 get queue=transcode-result requeue=false | grep transcode-result |  sed "s/.* | \({.*} \).*|/\1/g"|python -m json.tool|grep md5|sed "s/.*md5\": \"\([0-9a-fA-F]*\)\".*/\1/g")
 echo $result
 
